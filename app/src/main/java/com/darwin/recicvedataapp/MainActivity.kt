9bc02791c36de62160d8c509b9d7261d7bac7f24
tmblr1021l0036 <bishal.singh.3572@gmail.com>
2022-05-02 19:07:58 +0530

    Initial commit

---
+++ app/src/main/java/com/darwin/recicvedataapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.darwin.recicvedataapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.darwin.recicvedataapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding : ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    handleSendText(intent) // Handle text being sent
+                } else if (intent.type?.startsWith("image/") == true) {
+                    handleSendImage(intent) // Handle single image being sent
+                }
+            }
+            intent?.action == Intent.ACTION_SEND_MULTIPLE
+                    && intent.type?.startsWith("image/") == true -> {
+                handleSendMultipleImages(intent) // Handle multiple images being sent
+            }
+            else -> {
+                // Handle other intents, such as being started from the home screen
+            }
+        }
+    }
+
+    private fun handleSendText(intent: Intent) {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            binding?.demotext?.text = it.toString()
+        }
+    }
+
+    private fun handleSendImage(intent: Intent) {
+        (intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+            Log.d("LogTag2", "handleSendImage: $it")
+        }
+    }
+
+    private fun handleSendMultipleImages(intent: Intent) {
+        intent.getParcelableArrayListExtra<Parcelable>(Intent.EXTRA_STREAM)?.let {
+            Log.d("LogTag3", "handleSendMultipleImages: $it")
+        }
+    }
+}
